8138ce804fc2e5d44120b9a467459a61bb6b5df7
GGN_2015 <premierbob@qq.com>
2024-08-22 00:53:13 +0800

    feat:command_line_argv

--- README.md
+++ README.md
@@ -1,3 +1,23 @@
 # random_cursor_image
 generating random PNG images with randomly positioned cursors.
 
+
+
+## dependencies
+
+- `python>3.10`, `pillow`, `tqdm`
+
+
+
+## usage
+
+```bash
+python3 -m random_cursor_image <IMAGE_SAVE_PATH>
+python3 -m random_cursor_image -r <RANDOM_SEED> <IMAGE_SAVE_PATH>
+```
+
+
+
+## sample image
+
+![](./sample_image.png)
--- random_cursor_image/__main__.py
+++ random_cursor_image/__main__.py
@@ -1,10 +1,32 @@
 from .img_creator import random_cursor_image
-import os
+import json
+import time
+import sys
+
+
 
 def main():
-    dirnow = os.path.dirname(os.path.abspath(__file__))
-    savepath = os.path.join(dirnow, "..", "sample_image.png")
-    random_cursor_image(1145141919810, savepath)
+    sys_argv = json.loads(json.dumps(sys.argv[1:]))
+
+    if len(sys_argv) not in [1, 3]:
+
+        raise AssertionError("argv length incorrect.")
+    if len(sys_argv) == 1: # usage1: python3 -m random_cursor_image <IMAGE_SAVE_PATH>
+        save_path = sys_argv[0]
+        random_cursor_image(int(time.time() * (10**6)), save_path)
+        exit(0)
+
+    if len(sys_argv) == 3: # usage2: python3 -m random_cursor_image -r <RANDOM_SEED> <IMAGE_SAVE_PATH>
+        if sys_argv[0] != "-r":
+            raise AssertionError("first argv must be '-r'.")
+        random_seed = int(sys_argv[1])
+        save_path   = sys_argv[2]
+        random_cursor_image(random_seed, save_path)
+        exit(0)
+
+    raise AssertionError("unimplemented.")
+
+
 
 if __name__ == "__main__":
     main()
